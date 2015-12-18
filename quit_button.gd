62891c18a58ae2029c5b2b27c1224cd249647dd6
AbChCa <Heybye44@gmail.com>
2015-12-17 21:20:56 -0500

    Lots of fixes and changes

--- quit_button.gd
+++ quit_button.gd
@@ -10,11 +10,15 @@ func _ready():
 	pass
 
 func _on_Button_pressed():
-	print(get_tree().get_current_scene())
 	
-	if(get_tree().get_current_scene() != "res://Scenes/start_menu.gd"):
-		get_tree().change_scene("res://Scenes/start_menu.gd")
+	if(not (get_tree().get_current_scene().get_path() == "res://Scenes/start_menu.scn")):
+		get_tree().change_scene("res://Scenes/start_menu.scn")
 	else:
 		get_tree().quit()
 	pass
 
+
+
+func _on_Button_pressed_on_start_menu():
+	get_tree().quit()
+	pass
